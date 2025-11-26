b65c3ddb1b9bc46e5f32d9083410282ff98c8e4f
sunildadheech <77289380+sunildadheech@users.noreply.github.com>
2025-11-26 11:23:52 -0600

    Update and rename ArrayList.java to ArrayListExample.java

---
+++ ArrayList1/ArrayListExample.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+
+public class ArrayListExample {
+    public static void main(String[] args) {
+        ArrayList<Integer> numbers = new ArrayList<>();
+        
+        // Add elements
+        numbers.add(10);
+        numbers.add(20);
+        numbers.add(30);
+        
+        // Print elements
+        System.out.println(numbers);
+    }
+}
